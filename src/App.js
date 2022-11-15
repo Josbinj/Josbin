a38c2003449d435f82ac62d9b5dcdd3297dd30f7
Josbinj <118161687+Josbinj@users.noreply.github.com>
2022-11-15 16:33:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import react , { Component}  from 'react';
-import         { render   }  from 'react-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav class="navbar background">
+        <ul class="nav-list">
+          <div class="logo">
+            <img src="" />
+          </div>
+          <li>
+            <a href="#courses">Co</a>
+          </li>
+        </ul>
+
+        <div class="rightNav">
+          <input type="text" name="search" id="search" />
+          <button class="btn btn-sm">Search</button>
+        </div>
+      </nav>
+
+      <section class="section">
+        <div class="box-main">
+          <div class="firstHalf">
+            <h1 class="text-big">2022</h1>
+            <p class="text-small">rjgoipsjgpoewr</p>
+          </div>
+        </div>
+      </section>
+
+      <footer className="footer">
+        <p className="text-footer"></p>
+      </footer>
     </div>
   );
 }
